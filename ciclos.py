71fbd556c1fe151b3aaf36197104752a3701b68f
Adrian Caraballo <carabalapp@gmail.com>
2024-10-03 22:21:47 -0400

    nuevos archivos

--- ciclos.py
+++ ciclos.py
@@ -1,1 +1,29 @@
 #  Ciclos ( o bucles )
+
+# Son estruxcturas de control que permiten ejecutar un bloque de codigo repetidamente mientras se cumpla una condicion
+
+#  Hay 2 tipos principales de ciclos en python -> "for" y el "while"
+
+# 1. Ciclo "for"
+
+frutas = ["manzana", "cereza", "pera", "patilla"]
+
+for fruta in frutas:
+    print(fruta) # Imprime cada fruta de la lista
+    
+for i in range(5):
+    print(i) # Imprime los numeros del 0 al 4
+
+# Calculando los numeros pares hasta el 100
+
+for numero in range(1,100): # Comienza en 1 y termina en 100
+    if numero % 2 == 0:
+        print(numero, 'Es par')
+        
+#  2. Ciclo "while" se va a ejecutar un bloque de codigo mientras una condicion sea verdadera
+
+contador = 0
+
+while contador < 5:
+    print(contador)
+    contador = contador + 1
--- tipos_de_variables.py
+++ tipos_de_variables.py
@@ -37,7 +37,11 @@ print(type(falso))
 lista = [1,4,7,2,3.3]
 
 
+print(lista[0])
+print(lista[1])
 print(lista[2])
+print(lista[3])
+print(lista[4])
 
 lista[2] = 8
 
